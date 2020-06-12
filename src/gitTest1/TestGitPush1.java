fcd052781231c7561cd14fcfad3f536a4d190ea5
fatihbyrktrr <fatihbayraktar994@gmail.com>
2020-06-11 20:46:17 -0500

    changed

--- src/gitTest1/TestGitPush1.java
+++ src/gitTest1/TestGitPush1.java
@@ -1,8 +1,16 @@
 package gitTest1;
 
+import java.util.Scanner;
+
 public class TestGitPush1 {
 	public static void main(String[] args) {
 		System.out.println("Syntax Technologies");
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("write number");
+		
+		int x = scan.nextInt();
+		System.out.println("you good");
 		
 	}
 
